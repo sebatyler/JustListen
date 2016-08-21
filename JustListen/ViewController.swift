6def1e8905e8f12b6a62cb887e0102d5a79a25bb
sebatyler <sebatyler@gmail.com>
2016-08-21 17:29:23 +0900

    load data from json api

--- JustListen/ViewController.swift
+++ JustListen/ViewController.swift
@@ -8,15 +8,29 @@
 
 import UIKit
 import youtube_ios_player_helper
+import Alamofire
+
 
 class ViewController: UIViewController {
     @IBOutlet weak var playerView: YTPlayerView!
+    var playlist: NSArray?
+    var url: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let playerVars: [String: AnyObject] = ["origin": "http://www.youtube.com", "playsinline": 1]
-        self.playerView.loadWithVideoId("rUSddpvB4X0", playerVars: playerVars)
+        Alamofire.request(.GET, "https://www.letmedoctor.com/playlist.json").validate().responseJSON { response in
+            switch response.result {
+            case .Success(let JSON):
+                self.playlist = JSON.objectForKey("playlist") as? NSArray
+                self.url = JSON.objectForKey("url") as? String
+                let videoId = self.playlist?.objectAtIndex(0).objectForKey("id") as? String
+                let playerVars: [String: AnyObject] = ["origin": "http://www.youtube.com", "playsinline": 1]
+                self.playerView.loadWithVideoId(videoId!, playerVars: playerVars)
+            case .Failure(let error):
+                print(error)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
