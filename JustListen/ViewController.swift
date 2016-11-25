43a12f3109a1d6a6e07f2e1c6969c9c1096b51d0
sebatyler <sebatyler@gmail.com>
2016-11-26 00:26:31 +0900

    alamofire migration to version 4

--- JustListen/ViewController.swift
+++ JustListen/ViewController.swift
@@ -44,15 +44,15 @@ class ViewController: UIViewController, YTPlayerViewDelegate {
     var url: String?
     var playlistIdx: Int? = -1
     var repeatSong = false
-    let playerVars: [String: AnyObject] = ["origin": "http://www.youtube.com", "playsinline": 1]
+    let playerVars: [String: AnyObject] = ["origin": "http://www.youtube.com" as AnyObject, "playsinline": 1 as AnyObject]
     let playlistUrl = "https://www.letmedoctor.com/playlist.json"
 
-    @IBAction func changeRepeatSwitch(sender: UISwitch) {
-        self.repeatSong = sender.on
+    @IBAction func changeRepeatSwitch(_ sender: UISwitch) {
+        self.repeatSong = sender.isOn
     }
     
-    @IBAction func changePlayerControl(sender: UISegmentedControl) {
-        title = sender.titleForSegmentAtIndex(sender.selectedSegmentIndex)
+    @IBAction func changePlayerControl(_ sender: UISegmentedControl) {
+        title = sender.titleForSegment(at: sender.selectedSegmentIndex)
         
         guard let title = title else {
             return
@@ -67,7 +67,7 @@ class ViewController: UIViewController, YTPlayerViewDelegate {
             self.playerView.playVideo()
         case "Stop":
             self.playerView.stopVideo()
-            self.playerControl.setTitle("Play", forSegmentAtIndex: 1)
+            self.playerControl.setTitle("Play", forSegmentAt: 1)
         case "Next":
             self.loadSong()
         default:
@@ -85,31 +85,33 @@ class ViewController: UIViewController, YTPlayerViewDelegate {
         self.loadGoogleAdBanner()
     }
     
-    private func loadGoogleAdBanner() {
+    fileprivate func loadGoogleAdBanner() {
         bannerView.adUnitID = "ca-app-pub-6928022194889148/9093399514"
         bannerView.rootViewController = self
-        bannerView.loadRequest(GADRequest())
+        bannerView.load(GADRequest())
     }
     
-    private func getPlaylist() {
-        Alamofire.request(.GET, self.playlistUrl).validate().responseJSON { response in
+    fileprivate func getPlaylist() {
+        Alamofire.request(self.playlistUrl, method: .get).validate().responseJSON { response in
             switch response.result {
-            case .Success(let JSON):
+            case .success(let JSON as [String: Any]):
+                debugPrint(JSON)
                 
                 guard let playlist = JSON["playlist"] as? [AnyObject] else {
                     return
                 }
                 
                 self.playlist = playlist.shuffled
-                self.url = JSON.objectForKey("url") as? String
+                self.url = JSON["url"] as? String
                 self.loadSong()
-            case .Failure(let error):
+            case .failure(let error):
                 print(error)
+            default: break
             }
         }
     }
     
-    private func loadSong(next: Bool = true) {
+    fileprivate func loadSong(_ next: Bool = true) {
         if var idx = playlistIdx {
             idx += next ? 1 : -1
             if (idx < 0) {
@@ -119,18 +121,18 @@ class ViewController: UIViewController, YTPlayerViewDelegate {
             
             if let videoId = self.playlist?[idx]["id"] as? String {
                 self.playlistIdx = idx
-                self.playerView.loadWithVideoId(videoId, playerVars: self.playerVars)
+                self.playerView.load(withVideoId: videoId, playerVars: self.playerVars)
             }
         }
     }
     
-    func playerViewDidBecomeReady(playerView: YTPlayerView) {
+    func playerViewDidBecomeReady(_ playerView: YTPlayerView) {
         playerView.playVideo()
     }
     
-    func playerView(playerView: YTPlayerView, didChangeToState state: YTPlayerState) {
-        if (state == YTPlayerState.Ended) {
-            self.playerControl.setTitle("Play", forSegmentAtIndex: 1)
+    func playerView(_ playerView: YTPlayerView, didChangeTo state: YTPlayerState) {
+        if (state == YTPlayerState.ended) {
+            self.playerControl.setTitle("Play", forSegmentAt: 1)
             if (self.repeatSong) {
                 self.playerView.playVideo()
             }
@@ -138,19 +140,19 @@ class ViewController: UIViewController, YTPlayerViewDelegate {
                 self.loadSong()
             }
         }
-        else if (state == YTPlayerState.Playing) {
-            if (self.playerControl.titleForSegmentAtIndex(1) != "Pause") {
-                self.playerControl.setTitle("Pause", forSegmentAtIndex: 1)
+        else if (state == YTPlayerState.playing) {
+            if (self.playerControl.titleForSegment(at: 1) != "Pause") {
+                self.playerControl.setTitle("Pause", forSegmentAt: 1)
             }
         }
-        else if (state == YTPlayerState.Paused) {
-            if (self.playerControl.titleForSegmentAtIndex(1) != "Play") {
-                self.playerControl.setTitle("Play", forSegmentAtIndex: 1)
+        else if (state == YTPlayerState.paused) {
+            if (self.playerControl.titleForSegment(at: 1) != "Play") {
+                self.playerControl.setTitle("Play", forSegmentAt: 1)
             }
         }
     }
     
-    private func enableBackgroundPlay() {
+    fileprivate func enableBackgroundPlay() {
         do {
             try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
             do {
--- Podfile
+++ Podfile
@@ -7,7 +7,7 @@ target 'JustListen' do
 
   # Pods for JustListen
   pod "youtube-ios-player-helper", "~> 0.1.4"
-  pod 'Alamofire', '~> 3.4'
+  pod 'Alamofire', '~> 4.0'
 
   # admob
   pod 'Firebase/Core'
