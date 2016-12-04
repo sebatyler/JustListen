d5859a485378dc53dad6c11c22595eee297d3d62
sebatyler <sebatyler@gmail.com>
2016-12-05 02:16:21 +0900

    keep playing when become inactive state

--- JustListen/AppDelegate.swift
+++ JustListen/AppDelegate.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import youtube_ios_player_helper
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var isPlaying = false
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
@@ -22,11 +24,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        isPlaying = getViewController().playerView.playerState() == YTPlayerState.playing
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        if isPlaying {
+            let playerView = getViewController().playerView!
+            if playerView.playerState() == YTPlayerState.paused {
+                playerView.playVideo()
+            }
+        }
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -40,7 +49,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    private func getViewController() -> ViewController {
+        return window?.rootViewController as! ViewController
+    }
 }
 
